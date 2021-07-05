ab9b714d4e11beb6015af339ad2a1ef883605ce1
kaminchu <kmchu.mail@gmail.com>
2021-07-05 18:20:53 +0900

    install

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+apt-get install -y git build-essential sqlite3-dev
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
 CONFIGURE_OPTS='--disable-install-rdoc' rbenv install 3.0.1
