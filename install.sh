e0c542fd7b86e9e61acd22b95b47e04d887da3a9
kaminchu <kmchu.mail@gmail.com>
2021-07-05 18:51:20 +0900

    fix

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 apt-get update && apt-get install -y git build-essential libsqlite3-dev rbenv
 
 CONFIGURE_OPTS='--disable-install-rdoc' rbenv install 3.0.1
+eval "$(rbenv init -)"
 gem install rails -v 6.0.3
 bundle install
 yarn install
