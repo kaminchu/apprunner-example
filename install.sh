784eed4d262e98af1519e18ee96734d257c713cd
kaminchu <kmchu.mail@gmail.com>
2021-07-27 18:32:50 +0900

    aaa

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 printenv
 which curl
 which wget
-apk add curl
+yum -y install git
 apt-get update && apt-get install -y git build-essential libsqlite3-dev rbenv
 
 CONFIGURE_OPTS='--disable-install-rdoc' rbenv install 3.0.1
