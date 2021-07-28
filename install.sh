07842ad42364973bd9730e0548fd4253c4f2604d
kaminchu <kmchu.mail@gmail.com>
2021-07-28 13:54:19 +0900

    aaa

--- install.sh
+++ install.sh
@@ -6,6 +6,7 @@ printenv
 curl ${ECS_CONTAINER_METADATA_URI_V4}/task
 
 amazon-linux-extras install -y ruby2.6
+yum install -y ruby-devel rpm-build git gcc gcc-c++ zlib-devel openssl-devel rubygems-devel
 gem install rails -v 6.0.3
 bundle install
 yarn install
