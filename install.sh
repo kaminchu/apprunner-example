48f92d5b118687bcfa13143f3743e3e36cd0ed8e
kaminchu <kmchu.mail@gmail.com>
2021-07-28 10:31:11 +0900

    aaaa

--- install.sh
+++ install.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 set -eux
 
-cat /etc/os-release
-printenv
-# yum install -y git gcc openssl-devel readline-devel zlib-devel
-# git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-# export PATH="$HOME/.rbenv/bin:$PATH"
-# eval "$(rbenv init -)"
-# exec $SHELL -l
-# mkdir -p "$(rbenv root)"/plugins
-# git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+# cat /etc/os-release
+# printenv
+# # yum install -y git gcc openssl-devel readline-devel zlib-devel
+# # git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+# # export PATH="$HOME/.rbenv/bin:$PATH"
+# # eval "$(rbenv init -)"
+# # exec $SHELL -l
+# # mkdir -p "$(rbenv root)"/plugins
+# # git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
 
-curl $ECS_CONTAINER_METADATA_URI_V4/task
+# curl $ECS_CONTAINER_METADATA_URI_V4/task
 # /usr/local/rbenv/bin/rbenv install 3.0.1
 # eval "$(/usr/local/rbenv/bin/rbenv init -)"
 gem install rails -v 6.0.3
