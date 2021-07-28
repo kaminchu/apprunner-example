b66229ede5a06e72714c543aeea48062fff057e5
kaminchu <kmchu.mail@gmail.com>
2021-07-28 11:15:03 +0900

    aa

--- install.sh
+++ install.sh
@@ -1,19 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
-# cat /etc/os-release
+cat /etc/os-release
 printenv
-# # yum install -y git gcc openssl-devel readline-devel zlib-devel
-# # git clone https://github.com/rbenv/rbenv.git ~/.rbenv
-# # export PATH="$HOME/.rbenv/bin:$PATH"
-# # eval "$(rbenv init -)"
-# # exec $SHELL -l
-# # mkdir -p "$(rbenv root)"/plugins
-# # git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+curl ${ECS_CONTAINER_METADATA_URI_V4}/task
 
-# curl $ECS_CONTAINER_METADATA_URI_V4/task
-# /usr/local/rbenv/bin/rbenv install 3.0.1
-# eval "$(/usr/local/rbenv/bin/rbenv init -)"
 yum install -y ruby
 gem install rails -v 6.0.3
 bundle install
